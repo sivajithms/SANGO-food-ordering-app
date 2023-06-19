6b7545efc4c15835a6af9f6b5eac70e4c38a4bd1
sivajithms <sivajithm@gmail.com>
2023-06-19 14:50:35 +0530

    auth made short and clean

--- API/Auth/index.js
+++ API/Auth/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 
 import { UserModel } from '../../database/allModels';
 
@@ -16,30 +14,15 @@ Method          POST
 
 Router.post('/signup', async (req, res) => {
     try {
-        const { email, password, fullname, phoneNumber } = req.body.credentials;
-
-        //check whether email or phone number exists
-        const checkUserByEmail = await UserModel.findOne({ email });
-        const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-
-        if (checkUserByEmail || checkUserByPhone) {
-            return res.json({ error: "user already exists" });
-        }
-
-        //hashing and salting
-        const bcryptSalt = await bcrypt.genSalt(8);
-        const hashPassword = await bcrypt.hash(password, bcryptSalt);
+        await UserModel.findEmailAndPhone(req.body.credentials);
 
         //DB
-        await UserModel.create({
-            ...req.body.credentials,
-            password: hashPassword
-        })
+        const newUser = await UserModel.create(req.body.credentials)
 
         //JWT token
-        const token = jwt.sign({ user: { fullname, email } }, "SangoApp")
+        const token = newUser.generateJwtToken();
 
-        return res.status(200).json({token});
+        return res.status(200).json({ token });
 
     } catch (error) {
         return res.status(500).json({ error: error.message })
--- database/user/index.js
+++ database/user/index.js
@@ -1,11 +1,50 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String },
     address: [{ detail: { type: String }, for: { type: String } }],
-    phoneNumber:[{type:Number}]
-},{timestamps:true});
+    phoneNumber: [{ type: Number }]
+}, { timestamps: true });
+
+UserSchema.statics.findEmailAndPhone = async ({ email, phoneNumber }) => {
+
+    //check whether email or phone number exists
+    const checkUserByEmail = await UserModel.findOne({ email });
+    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+    if (checkUserByEmail || checkUserByPhone) {
+        throw new Error("user already exists");
+    }
+    return false;
+}
+
+UserSchema.methods.generateJwtToken = function () {
+    return jwt.sign({ user: this._id.toString() }, "SangoApp")
+}
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    //password is not modified
+    if (!user.isModified('password')) return next();
+
+    //generating bcrypt salt
+    bcrypt.genSalt(8, (error, salt) => {
+        if (error) return next(error);
+
+        //hashing the password
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error);
+
+            //assigned hashed password
+            user.password = hash;
+            return next();
+        })
+    })
+})
 
 export const UserModel = mongoose.model("Users", UserSchema);
